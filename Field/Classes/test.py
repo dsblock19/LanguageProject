ce8b4741956e58c5a46970a086823eff53f8239e
Dan Block <dsblock19@gmail.com>
2020-06-20 01:20:43 -0400

    Update test.py

--- Field/Classes/test.py
+++ Field/Classes/test.py
@@ -2,8 +2,13 @@ from WordGenInky import WordGen
 
 x = WordGen
 
+'''
 while True:
     prog = input('Program: ')
     if prog == 'WordGen':
         x.NewFam()
         print('')
+'''
+
+while True:
+        x.NewFam()
