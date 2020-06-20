59f2579721167d6160e3159a2e2aa8c7310f6437
Dan Block <dsblock19@gmail.com>
2020-06-20 15:46:48 -0400

    Update test.py

--- Field/Classes/test.py
+++ Field/Classes/test.py
@@ -1,15 +1,14 @@
-#from MultiFamWordGenInky import WordGen
+from MultiFamWordGenInky import WordGen
 from StoConversion import StoConversion
 
 x = StoConversion
+y = WordGen
 
-'''
+
 while True:
     prog = input('Program: ')
     if prog == 'WordGen':
         x.NewFam()
         print('')
-'''
-
-while True:
-        x.InSto()
+    elif prog == 'Conversion':
+        y.InSto()
