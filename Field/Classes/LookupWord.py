9eef76906296eda46f0920689b0afbfda06e84dd
Dan Block <dsblock19@gmail.com>
2020-07-08 21:26:52 -0400

    Update LookupWord.py

--- Field/Classes/LookupWord.py
+++ Field/Classes/LookupWord.py
@@ -31,7 +31,8 @@ class LookUp():
                     self.cur.execute(sql)
                     results = self.cur.fetchone()
                     self.con.commit()
-                    print(str(results))
+                    for i in results:
+                        print(str(results[i]))
                     root = str(word)
                     root = root.upper()
                     #Font Specific Change
