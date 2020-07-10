1194a709692a6127b160c2d9b712f26860e70dce
Dan Block <dsblock19@gmail.com>
2020-07-10 17:31:04 -0400

    Update MultiFamWordGenInky.py

--- Field/Classes/MultiFamWordGenInky.py
+++ Field/Classes/MultiFamWordGenInky.py
@@ -15,6 +15,7 @@ class WordGen():
         self.img = Image.new("P", (400, 300))
         self.clear = Image.new("P", (400, 300))
         self.draw = ImageDraw.Draw(self.img)
+        self.dclear = ImageDraw.Draw(self.clear)
         self.stofontpath = '/home/pi/LanguageProject/Field/Classes/Fonts/sto-ith/sto-ith.ttf'
         self.stofont = ImageFont.truetype(self.stofontpath, 41)
         self.x = 50
@@ -421,9 +422,9 @@ class WordGen():
         eink = input('Inky? ')
         eink = eink.upper()
         if eink == 'YES':
-            self.inky_display.set_image(self.clear)
-            self.inky_display.show()
-            
+            self.inky_display.clear()
+
+
             self.draw.text((self.x, self.y), stomessage, self.inky_display.RED, self.stofont)
             #flipped = img.rotate(90)
             #inky_display.set_image(flipped)
