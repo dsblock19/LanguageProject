a64f597c96b441afd9f393967ac7ff8247e6d79d
Dan Block <dsblock19@gmail.com>
2020-07-23 15:02:45 -0400

    Update CSV.py

--- Field/CSV.py
+++ Field/CSV.py
@@ -1,7 +1,7 @@
 import csv
 import pymysql
 
-con = pymysql.connect('localhost', 'dblo', '1819Kirk!', 'dictionary')
+con = pymysql.connect('localhost', 'dblo', '1819Kirk!', 'Sto')
 cur = con.cursor()
 
 with open('/home/pi/LanguageProject/words.csv', 'r') as Dcsv:
@@ -9,7 +9,7 @@ with open('/home/pi/LanguageProject/words.csv', 'r') as Dcsv:
     for row in Dreader:
         print(row[0] + ', ' + row[1] + ', ' + row[2])
 
-        sql = "INSERT INTO wordsII VALUES('" + str(row[0]) + "','" + str(row[1]) + "','" + str(row[2]) + "');"
+        sql = "INSERT INTO FoundationalFamilies VALUES('" + str(row[0]) + "','" + str(row[1]) + "','" + str(row[2]) + "');"
         try:
             cur.execute(sql)
             con.commit()
@@ -17,4 +17,3 @@ with open('/home/pi/LanguageProject/words.csv', 'r') as Dcsv:
         except Exception as e:
             con.rollback()
             print(' Error: ' + e)
-        
