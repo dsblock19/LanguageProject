d6b54631fbd8b59a107a34c9f8fbc8e411cfd7cd
Dan Block <dsblock19@gmail.com>
2020-07-23 10:28:18 -0400

    Update CSV.py

--- Field/CSV.py
+++ Field/CSV.py
@@ -1,25 +1,6 @@
-#Libraries
-import pymysql
+import csv
 
-con = pymysql.connect('localhost', 'dblo', '1819Kirk!', 'dictionary')
-cur = con.cursor()
-
-results = []
-sql = "SELECT * FROM words;"
-
-with con:
-    try:
-        cur.execute(sql)
-        results = cur.fetchall()
-        con.commit()
-        for i in range(len(results)):
-            stof = open('/home/pi/LanguageProject/Output/Dictionary.csv', 'a')
-            res = str(results[i])
-            res = res.replace("'" , '')
-            res = res.replace('(' , '' )
-            res = res.replace(')' , '' )
-            stof.write(res + '\n')
-            stof.close()
-    except Exception as e:
-        con.rollback()
-        print(e)
+with open('~/LanguageProject/dictionary.csv', 'r') as Dcsv:
+    Dreader = csv.reader(Dcsv)
+    for row in Dreader:
+        print(row)
