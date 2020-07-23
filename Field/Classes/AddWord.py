41c4edb411d20dfc66a8c3ac63db57a884fff14e
Dan Block <dsblock19@gmail.com>
2020-07-23 15:13:10 -0400

    Changed SQL Python Code

--- Field/Classes/AddWord.py
+++ Field/Classes/AddWord.py
@@ -27,47 +27,47 @@ class Add2Dic():
         defin = input('Definition: ')
         part = input('Part of Speech: ')
         sql = "INSERT INTO FoundationalFamilies VALUES('" + str(word) + "','" + str(defin) + "','" + str(part) + "');"
-        with self.con:
-            try:
-                self.cur.execute(sql)
-                self.con.commit()
-                print(' Success')
+        #with self.con:
+        try:
+            self.cur.execute(sql)
+            self.con.commit()
+            print(' Success')
 
-                root = word
-                root = root.upper()
-                #Font Specific Change
-                #ST --> F
-                root = root.replace('ST', 'F')
-                #OO --> L
-                root = root.replace('OO', 'L')
-                #SH --> Q
-                root = root.replace('SH', 'Q')
-                #AW --> W
-                root = root.replace('AW', 'W')
-                #I:I --> Y
-                root = root.replace('I:I', 'Y')
-                #CH --> @
-                root = root.replace('CH', '@')
-                #TH --> #
-                root = root.replace('TH', '#')
-                #' -->
-                root = root.replace("'", "")
+            root = word
+            root = root.upper()
+            #Font Specific Change
+            #ST --> F
+            root = root.replace('ST', 'F')
+            #OO --> L
+            root = root.replace('OO', 'L')
+            #SH --> Q
+            root = root.replace('SH', 'Q')
+            #AW --> W
+            root = root.replace('AW', 'W')
+            #I:I --> Y
+            root = root.replace('I:I', 'Y')
+            #CH --> @
+            root = root.replace('CH', '@')
+            #TH --> #
+            root = root.replace('TH', '#')
+            #' -->
+            root = root.replace("'", "")
 
-                print('')
-                eink = input('Inky? ')
-                eink = eink.upper()
-                if eink == 'YES':
-                    self.img = Image.new("P", (400, 300))
-                    self.draw = ImageDraw.Draw(self.img)
+            print('')
+            eink = input('Inky? ')
+            eink = eink.upper()
+            if eink == 'YES':
+                self.img = Image.new("P", (400, 300))
+                self.draw = ImageDraw.Draw(self.img)
 
-                    self.draw.text((self.x, self.y), root, self.inky_display.RED, self.stofont)
-                    #flipped = img.rotate(90)
-                    #inky_display.set_image(flipped)
-                    self.inky_display.set_border(self.inky_display.WHITE)
-                    self.inky_display.set_image(self.img)
-                    self.inky_display.show()
+                self.draw.text((self.x, self.y), root, self.inky_display.RED, self.stofont)
+                #flipped = img.rotate(90)
+                #inky_display.set_image(flipped)
+                self.inky_display.set_border(self.inky_display.WHITE)
+                self.inky_display.set_image(self.img)
+                self.inky_display.show()
 
-            except Exception as e:
-                self.con.rollback()
-                print(e)
+        except Exception as e:
+            self.con.rollback()
+            print(e)
         print('')
--- Field/Classes/LookupEngWord.py
+++ Field/Classes/LookupEngWord.py
@@ -25,48 +25,48 @@ class LookUpEng():
         print('')
         word = input('Word: ')
         sql = "SELECT ALL * FROM FoundationalFamilies WHERE definition LIKE '%" + word + "%';"
-        with self.con:
-            try:
-                self.cur.execute(sql)
-                results = self.cur.fetchall()
-                self.con.commit()
-                for i in range(len(results)):
-                    print(results[i])
-                root = results[0]
-                root = root[0]
-                root = root.upper()
-                #Font Specific Change
-                #ST --> F
-                root = root.replace('ST', 'F')
-                #OO --> L
-                root = root.replace('OO', 'L')
-                #SH --> Q
-                root = root.replace('SH', 'Q')
-                #AW --> W
-                root = root.replace('AW', 'W')
-                #I:I --> Y
-                root = root.replace('I:I', 'Y')
-                #CH --> @
-                root = root.replace('CH', '@')
-                #TH --> #
-                root = root.replace('TH', '#')
-                #' -->
-                root = root.replace("'", "")
+        #with self.con:
+        try:
+            self.cur.execute(sql)
+            results = self.cur.fetchall()
+            self.con.commit()
+            for i in range(len(results)):
+                print(results[i])
+            root = results[0]
+            root = root[0]
+            root = root.upper()
+            #Font Specific Change
+            #ST --> F
+            root = root.replace('ST', 'F')
+            #OO --> L
+            root = root.replace('OO', 'L')
+            #SH --> Q
+            root = root.replace('SH', 'Q')
+            #AW --> W
+            root = root.replace('AW', 'W')
+            #I:I --> Y
+            root = root.replace('I:I', 'Y')
+            #CH --> @
+            root = root.replace('CH', '@')
+            #TH --> #
+            root = root.replace('TH', '#')
+            #' -->
+            root = root.replace("'", "")
 
-                print('')
-                eink = input('Inky? ')
-                eink = eink.upper()
-                if eink == 'YES':
-                    self.img = Image.new("P", (400, 300))
-                    self.draw = ImageDraw.Draw(self.img)
+            print('')
+            eink = input('Inky? ')
+            eink = eink.upper()
+            if eink == 'YES':
+                self.img = Image.new("P", (400, 300))
+                self.draw = ImageDraw.Draw(self.img)
 
-                    self.draw.text((self.x, self.y), root, self.inky_display.RED, self.stofont)
-                    #flipped = img.rotate(90)
-                    #inky_display.set_image(flipped)
-                    self.inky_display.set_border(self.inky_display.WHITE)
-                    self.inky_display.set_image(self.img)
-                    self.inky_display.show()
-            except Exception as e:
-                self.con.rollback()
-                print(e)
+                self.draw.text((self.x, self.y), root, self.inky_display.RED, self.stofont)
+                #flipped = img.rotate(90)
+                #inky_display.set_image(flipped)
+                self.inky_display.set_border(self.inky_display.WHITE)
+                self.inky_display.set_image(self.img)
+                self.inky_display.show()
+        except Exception as e:
+            self.con.rollback()
+            print(e)
         print('')
