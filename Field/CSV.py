af5e64c4187a8c7076ac24a08070c50a95fdbf91
Dan Block <dsblock19@gmail.com>
2020-07-23 10:30:11 -0400

    Update CSV.py

--- Field/CSV.py
+++ Field/CSV.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('~/LanguageProject/dictionary.csv', 'r') as Dcsv:
+with open('~/LanguageProject/words.csv', 'r') as Dcsv:
     Dreader = csv.reader(Dcsv)
     for row in Dreader:
         print(row)
