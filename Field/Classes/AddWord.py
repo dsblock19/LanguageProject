29ad80717bbe8d024f47780188e618199ac6c99d
Dan Block <dsblock19@gmail.com>
2020-07-24 15:11:22 -0400

    Update AddWord.py

--- Field/Classes/AddWord.py
+++ Field/Classes/AddWord.py
@@ -26,7 +26,7 @@ class Add2Dic():
         word = input('Word: ')
         defin = input('Definition: ')
         part = input('Part of Speech: ')
-        sql = "INSERT INTO FoundationalFamilies VALUES('" + str(word) + "','" + str(defin) + "','" + str(part) + "');"
+        sql = "INSERT INTO GenerationII VALUES('" + str(word) + "','" + str(defin) + "','" + str(part) + "','II');"
         #with self.con:
         try:
             self.cur.execute(sql)
