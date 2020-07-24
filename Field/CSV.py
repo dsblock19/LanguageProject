6ce5169ff1f66104761798b8dae5668c6cdf788e
Dan Block <dsblock19@gmail.com>
2020-07-24 15:07:47 -0400

    Update CSV.py

--- Field/CSV.py
+++ Field/CSV.py
@@ -13,6 +13,7 @@ with open('/home/pi/LanguageProject/FoundationalFamilies.csv', 'r') as Dcsv:
         print(row[0] + ', ' + row[1] + ', ' + row[2] + ', ' + row[3])
 
         sql = "INSERT INTO GenerationII VALUES('" + str(row[0]) + "','" + str(row[1]) + "','" + str(row[2]) + "','" + str(row[3]) + ");"
+        print(sql)
         try:
             cur.execute(sql)
             con.commit()
