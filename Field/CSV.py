d327999121cdc2beab63f3d8ba1b6a1f25b447f8
Dan Block <dsblock19@gmail.com>
2020-07-24 14:53:31 -0400

    Update CSV.py

--- Field/CSV.py
+++ Field/CSV.py
@@ -12,7 +12,7 @@ with open(csvFilePath, 'r') as Dcsv:
     for row in Dreader:
         print(row[0] + ', ' + row[1] + ', ' + row[2] + ', ' + row[3])
 
-        sql = "INSERT INTO " + str(wrdList) + "VALUES('" + str(row[0]) + "','" + str(row[1]) + "','" + str(row[2]) + "','" + str(row[3]) + ");"
+        sql = "INSERT INTO " + str(wrdList) + " VALUES('" + str(row[0]) + "','" + str(row[1]) + "','" + str(row[2]) + "','" + str(row[3]) + ");"
         try:
             cur.execute(sql)
             con.commit()
